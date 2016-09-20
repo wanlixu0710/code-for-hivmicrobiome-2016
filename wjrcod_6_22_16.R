ba619b89a578d4b54062a0a19cf593403fed592e
wanlixu0710 <wanlixu0710@gmail.com>
2016-09-19 22:05:52 -0400

    9_19_16_add in lmer

--- wjrcod_6_22_16.R
+++ wjrcod_6_22_16.R
@@ -161,7 +161,6 @@ summary(lm(clinical$LPS.plasma~clinical$group)) #pt is higher
 # fig2 <- ggplot(clinical.fig,  aes( y=as.numeric(value), x=factor(gender)))+ geom_boxplot(aes(colour=gender))+ scale_x_discrete()+ labs(x="male vs. female", y="antibody", title="antibody boxplot between male and female")+ facet_wrap(~variable, scales="free_y")+ theme_bw();fig2
 
 
-
 ### 2. Now we are moving to the patients only
 pt.a.div <- filter(a.div, group=="Patient")
 pt.otu <- filter(otu, Sample_ID %in% pt.a.div$Sample_ID)
@@ -173,13 +172,12 @@ pt.a.div.plot <- melt(pt.a.div, id.vars=c("Sample_ID","group","simpson","Race","
 fig2 <- ggplot(pt.a.div.plot,  aes( y=simpson, x=value)) + geom_point()+  geom_smooth(method="lm", se=FALSE)+  labs(x="value", y="simpson", title="titers and predictors vs. simpson")+ facet_wrap(~variable, scales="free")+ theme(axis.text.x = element_text(angle = 45)) 
 fig2
 
-
 ## 2.1 cut antibody titer to two levels for the graphing purpose
 table(pt.a.div$cd4.igg.dif)
-pt.a.div$cd4.igg <- cut(pt.a.div$cd4.igg.dif,c(-20,37,Inf), c("low","high"))
+pt.a.div$cd4.igg <- cut(pt.a.div$cd4.igg.dif,c(-Inf,37,Inf), c("low","high"))
 
 table(pt.a.div$dsdna.igg.dif)
-pt.a.div$dsdna.igg <- cut(pt.a.div$dsdna.igg.dif,c(-20,18,Inf), c("low","high"))
+pt.a.div$dsdna.igg <- cut(pt.a.div$dsdna.igg.dif,c(-Inf,18,Inf), c("low","high"))
 
 table(pt.a.div$ana.igg.dif)
 pt.a.div$ana.igg <- cut(pt.a.div$ana.igg.dif,c(-Inf,0.5,Inf), c("low","high"))
@@ -367,4 +365,55 @@ indi.data <- pt.L6.clinical %>% select(Sample_ID, TLR9.base) %>% filter(!is.na(T
 indi.L6 <- pt.L6.indi[rownames(pt.L6.indi) %in% indi.data$Sample_ID,]
 indi.sp <- multipatt(indi.L6, indi.data$TLR9.base, control = how(nperm=350))
 summary(indi.sp)
+
+#### 1. will use the titer increase as the outcome varible, and microbiome (a.div in this case) as predictor, firstly focusing on flu induced titer
+# change the formating to long format in the excel and read in R
+### lets focus on flu.igg, flu.igm, flu.iga first, and also bcell, cd3, cd4, h1n1, b, h3n2, these variables only have day 0 and day 14.
+pt.a.div.long <- read.csv(file="pt.a.div.long.csv")
+library(tidyr)
+pt.a.div.w <- spread(pt.a.div.long, variable, value)
+
+### 1.1 try with flu.igm using repeated measures of anova
+summary(with(pt.a.div.w, aov(flu.igm~time+simpson))) # it doesn't seem that you could model the intercept of the vaccine titer.
+
+### 1.2 choose the variables and run the correlations, HLM doesn't work for this type of data since it only has 2 time points.
+#cor1 <- pt.a.div %>% select(-1) %>% select(-(3:6)) %>% select(-(27:53)) #only flu vaccine induced antibody
+cor1 <- pt.a.div %>% select(-1) %>% select(-(3:6)) %>% select(-(51:53))
+cor2 <- rcorr(as.matrix(cor1), type="pearson")
+cor3 <- cbind(data.frame(cor2$r)[,(1:2)], data.frame(cor2$n)[,(1:2)], data.frame(cor2$P)[,(1:2)])
+write.csv(cor3, file="test.csv")
+# from the correlation matrix we found for simpson, h3n2d0(0.07), flu.iga.d0(0.08), flu.igg.fold(0.03), TLR2(0.07), TLR4(0.04); and for shannon, h3n2d0(0.054), h3n2.d14(0.08), flu.igg.fold(0.03), TLR2(0.07), TLR4(0.04)
+
+## then look at the pattern of the vaccine titers and simpson diversity
+pt.a.div.plot <- melt(pt.a.div, id.vars=c("Sample_ID","group","simpson","Race","age","gender","shannon"))
+fig1 <- ggplot(pt.a.div.plot,  aes( y=simpson, x=value)) + geom_point()+  geom_smooth(method="lm", se=FALSE)+  labs(x="value", y="simpson", title="titers and predictors vs. simpson")+ facet_wrap(~variable, scales="free")+ theme(axis.text.x = element_text(angle = 45));fig1
+
+fig2 <- ggplot(pt.a.div.plot,  aes( y=simpson, x=value)) + geom_point()+  geom_smooth()+  labs(x="value", y="simpson", title="titers and predictors vs. simpson")+ facet_wrap(~variable, scales="free")+ theme(axis.text.x = element_text(angle = 45));fig2
+
+### 1.2 try to model the titer with simpson diversity and other factor
+## 1.2.1 model h3n2.d0
+# select the variable that is correlated to h3n2.d0
+# cor4 <- data.frame(cor2$P)%>%select(h3n2.d0, flu.Iga.d0,flu.Igg.fold) %>% mutate(draw_rownames=row.names(cor2$P)) %>%filter(h3n2.d0<0.1|flu.Iga.d0<0.1|flu.Igg.fold<0.1)#this draw everthing
+data.frame(cor2$P)%>% mutate(draw_rownames=row.names(cor2$P)) %>%filter(h3n2.d0<0.1)%>%select(draw_rownames)
+summary(lm(h3n2.d0~simpson, data=pt.a.div))
+summary(lm(h3n2.d0~simpson+age, data=pt.a.div)) # better model?
+summary(lm(h3n2.d0~simpson+age+Race, data=pt.a.div))
+summary(lm(h3n2.d0~simpson+age+gender, data=pt.a.div))  
+
+#try to model h3n2.d14
+summary(lm(h3n2.d14~simpson, data=pt.a.div))
+
+## 1.2.1 model flu.Iga.d0
+data.frame(cor2$P)%>% mutate(draw_rownames=row.names(cor2$P)) %>%filter(flu.Iga.d0<0.1)%>%select(draw_rownames)
+summary(lm(flu.Iga.d0~simpson, data=pt.a.div))
+summary(lm(flu.Iga.d0~simpson+age, data=pt.a.div))
+summary(lm(flu.Iga.d0~simpson+gender, data=pt.a.div))# better model?
+
+
+####notes
+####we could group the patients and healthy together
+cor4 <- a.div %>% select(-1) %>% select(-(3:6)) %>% select(-(51:53))
+cor6 <- rcorr(as.matrix(cor4), type="pearson")
+cor7 <- cbind(data.frame(cor6$r)[,(1:2)], data.frame(cor6$n)[,(1:2)], data.frame(cor6$P)[,(1:2)])
+write.csv(cor7, file="test.csv")
 
